c9ad5febc9467f441dfee5c8a816ee243c2cf042
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-26 22:32:41 -0500

    Create activity-3.js

---
+++ activity-3.js
@@ -0,0 +1,19 @@
+// Activity 3: Color Changer
+// Goal: Use a for loop to cycle through a list of colors when a button is clicked.
+
+// Instructions
+  // In activity-3.html
+    // Create a div with an id = colorBox
+    // Create a button with an onclick = "changeColor()", this tells the button to run our function when we click it
+    // Add some stlyes to the div, including a default background
+
+  // Create a function called changeColor() that::
+    // Makes an array of colors called, colors.
+    // Uses a for loop to update which color comes next.
+    // Changes the background color of the <div> based on the color it.
+    // Resets back to the first color when reaching the end of the array.
+
+// Hints:
+  // - Use document.getElementById("colorBox").style.backgroundColor = color; to change colors.
+  // - Keep track of the current color index using a variable.
+  // - If the index reaches the last color, reset it to 0.
