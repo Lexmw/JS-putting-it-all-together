0057c6523faa27ed3128517275a733864d5f0d51
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-26 22:10:46 -0500

    Create activity-1.js

---
+++ activity-1.js
@@ -0,0 +1,13 @@
+// Activity 1: Click Counter Challenge (15 min)
+// Goal: Use event listeners, DOM manipulation, and conditionals to count button clicks.
+
+// Instructions:
+  // 1. Create a button labeled "Click Me" with an id "clickButton" in activity-1.html
+  // 2. Create a paragraph with an id="counter" in activity.html
+  // 3. Create a paragraph with an id="message" in activity.html
+  // 4. Write a JavaScript function that:
+          // - Increases the counter each time the button is clicked.
+          // - Updates the displayed number.
+          // - Shows a message when the count reaches 10.
+  // 5. Confirm this function is added as an onclick to the button.
+
