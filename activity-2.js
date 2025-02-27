8290934132233075d03bd39094ef9bb76a1878bd
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-26 22:21:40 -0500

    Create activity-2.js

---
+++ activity-2.js
@@ -0,0 +1,10 @@
+// Activity 2: Counting with Loops (15 min)
+// Goal: Use a for loop to generate a list of numbers dynamically in the DOM.
+
+//Instructions
+  // 1. Add a <button> with generateList() as an onclick to activity-2.html.
+  // 2. Add an empty <ul> with an id "numberList" to activity-2.html, this is where the numbers will appear.
+  // 3. Write a function called, "generateList" , that:
+    // Uses a for loop to count from 1 to 10.
+    // Creates a new <li> for each number.
+    // Adds each <li> to the <ul>.
